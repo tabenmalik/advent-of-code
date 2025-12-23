fd850e61dfe1c68d5ad792059f6367651e4898b2
Taben Malik <tabenjmalik@gmail.com>
2025-12-23 16:23:58 -0600

    simple script to fetch, cache, and print aoc inputs

--- src/aoc/aoc.py
+++ src/aoc/aoc.py
@@ -1,3 +1,7 @@
+import argparse
+import os
+import stat
+from pathlib import Path
 
 # aoc get-input [year] [day]
 # aoc run [year] [day]
@@ -14,6 +18,30 @@ def _setup_session(cookie_filename="cookies.txt"):
 
 def _download_input(year, day, ):
     resp = request.urlopen(f"https://adventofcode.com/{year}/day/{day}/input")
+    return resp.read().decode()
 
-def get_input():
-    ...
+
+def _get():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", type=int)
+    parser.add_argument("day", type=int)
+
+    args = parser.parse_args()
+
+    inp_filename = Path.cwd() / "inputs" / f"{args.year}" / f"{args.day:02d}.txt"
+    if inp_filename.exists():
+        with open(inp_filename) as fobj:
+            print(fobj.read())
+        return 0
+
+    _setup_session()
+    inp = _download_input(args.year, args.day)
+    inp_filename.parent.mkdir(mode=0o700, parents=True, exist_ok=True)
+    with open(inp_filename, mode="w") as fobj:
+        fobj.write(inp)
+    os.chmod(inp_filename, stat.S_IREAD)
+    print(inp)
+
+
+if __name__ == "__main__":
+    _get()
