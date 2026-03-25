14d1d4481e83687aa9ab3dd61be96c6c400469d6
Taben Malik <tabenjmalik@gmail.com>
2026-03-25 00:34:07 -0500

    simplify and test supporting aoc functions

---
+++ aoc.py
@@ -0,0 +1,56 @@
+"""
+Supporting functions for Advent of Code development.
+"""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+from urllib import request
+
+
+def xdg_cache_home() -> Path:
+    return Path(os.environ.get("XDG_CACHE_HOME", Path.home() / ".cache"))
+
+
+def xdg_data_home() -> Path:
+    return Path(os.environ.get("XDG_DATA_HOME", Path.home() / ".local" / "share"))
+
+
+def _fetch_input(year: int, day: int) -> str:
+    """
+    Download input data from website.
+
+    Requires an active session cookie header written to
+    `$XDG_DATA_HOME/aoc/session_cookie.txt`.
+    """
+    cookie_file = xdg_data_home() / "aoc" / "session_cookie.txt"
+    session_cookie = cookie_file.read_text().strip()
+    headers = {"Cookie": f"{session_cookie}", "User-Agent": "Taben Malik"}
+    req = request.Request(
+        f"https://www.adventofcode.com/{year}/day/{day}/input", headers=headers
+    )
+    resp = request.urlopen(req)
+    return resp.read().decode()
+
+
+def get_input(year: int, day: int) -> str:
+    """
+    Return the input text for a given day.
+
+    Input files are store at `$XDG_CACHE_HOME/aoc/`.
+    If an input file does not exist then attempts
+    to fetch and cache the input from adventofcode.com
+    """
+    cache_file = xdg_cache_home() / "aoc" / f"{year}-{day:02d}.txt"
+    if not cache_file.exists():
+        content = _fetch_input(year, day)
+        # use a tmp file to ensure atomic write of end dest
+        tmp_cache_file = cache_file.with_suffix(".tmp")
+        tmp_cache_file.parent.mkdir(exist_ok=True, parents=True)
+        tmp_cache_file.touch()
+        tmp_cache_file.chmod(0o600)
+        tmp_cache_file.write_text(content)
+        tmp_cache_file.replace(cache_file)
+
+    return cache_file.read_text()
---
+++ aoc_test.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import NamedTuple
+from unittest.mock import MagicMock
+
+import pytest
+
+import aoc
+
+
+class Fixture(NamedTuple):
+    cache: Path
+    data: Path
+    cookie_file: Path
+    urlopen_mock: MagicMock
+
+
+@pytest.fixture
+def getup(tmp_path, monkeypatch):
+    cache = tmp_path / "cache"
+    cache.mkdir()
+    monkeypatch.setenv("XDG_CACHE_HOME", str(cache))
+    cache = cache / "aoc"
+    cache.mkdir()
+
+    data = tmp_path / "data"
+    data.mkdir()
+    monkeypatch.setenv("XDG_DATA_HOME", str(data))
+    data = data / "aoc"
+    data.mkdir()
+
+    cookie_file = data / "session_cookie.txt"
+    cookie_file.write_text("session=1234abcd\n")
+
+    urlopen_mock = MagicMock()
+    urlopen_mock.return_value.status = 200
+    monkeypatch.setattr(aoc.request, "urlopen", urlopen_mock)
+
+    return Fixture(cache, data, cookie_file, urlopen_mock)
+
+
+def test_get_input_in_cache(getup):
+    cached_input = getup.cache / "2025-01.txt"
+    cached_input.write_text("hello\nworld\n!!\n")
+    problem_input = aoc.get_input(2025, 1)
+    assert problem_input == "hello\nworld\n!!\n"
+
+
+def test_get_input_successful_request(getup):
+    getup.urlopen_mock.return_value.read().decode.return_value = (
+        "this is downloaded input\n" "another line of input\n"
+    )
+
+    problem_input = aoc.get_input(2026, 20)
+    expected_cache_file = getup.cache / "2026-20.txt"
+    assert expected_cache_file.exists()
+    assert expected_cache_file.stat().st_mode & 0o777 == 0o600
+    assert expected_cache_file.read_text() == (
+        "this is downloaded input\n" "another line of input\n"
+    )
+    assert problem_input == ("this is downloaded input\n" "another line of input\n")
