477c84178838079a55971e259c0c84dfe8c4bc0d
Taben Malik <tabenjmalik@gmail.com>
2026-03-31 16:50:55 -0500

    solve 2015 day 4 part 2

---
+++ 2015/d4p2.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import hashlib
+from itertools import count
+
+import aoc
+
+
+def complete_key(secret_key: str) -> int:
+    # find the number (without leading zeros) such that
+    # the md5 hash of f"secret_key{number}" starts with 6 zeros
+    secret_key = secret_key.strip()
+    for num in count(1):
+        key = f"{secret_key}{num}"
+        md5_hash = hashlib.md5(key.encode("ascii"), usedforsecurity=False)
+        if md5_hash.hexdigest().startswith("000000"):
+            break
+    return num
+
+
+if __name__ == "__main__":
+    raise SystemExit(
+        aoc.problem_entry_point(complete_key, 2015, 4),
+    )
+
+
+import pytest  # noqa: E402
+
+
+@pytest.mark.parametrize(
+    "secret_key,md5_hash",
+    [
+        (aoc.get_input(2015, 4), 1038736),
+    ],
+)
+def test_complete_key(secret_key, md5_hash):
+    assert md5_hash == complete_key(secret_key)
