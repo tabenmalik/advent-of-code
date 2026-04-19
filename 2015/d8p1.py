32a28c9c10be146af5fea34e11810ccf313779d8
Taben Malik <tabenjmalik@gmail.com>
2026-04-19 15:47:26 -0500

    2015 day 8 tests with actual input

--- 2015/d8p1.py
+++ 2015/d8p1.py
@@ -7,7 +7,7 @@ import aoc
 RE_MEM_CHARS = re.compile(r'(\\\\|\\"|\\x[a-f0-9]{2}|[^"])')
 
 
-def char_diff(santa_list: str) -> str:
+def char_diff(santa_list: str) -> int:
     code_chars = 0
     mem_chars = 0
     for line in santa_list.strip().splitlines():
@@ -16,7 +16,8 @@ def char_diff(santa_list: str) -> str:
         code_points = RE_MEM_CHARS.findall(unquoted_line)
         mem_chars += len(code_points)
 
-    return str(code_chars - mem_chars)
+    diff = code_chars - mem_chars
+    return diff
 
 
 if __name__ == "__main__":
@@ -31,16 +32,17 @@ import pytest  # noqa: E402
 @pytest.mark.parametrize(
     "santa_list,result",
     [
-        ('""', "2"),
-        ('"abc"', "2"),
-        (r'"aaa\"aaa"', "3"),
-        (r'"\x27"', "5"),
-        (r'""\n"abc"\n"aaa\"aaa"\n"\x27"', "12"),
-        (r'"ab\x34\"five\\"', "7"),
-        (r'"\x2bv"', "5"),
-        (r'"\"', "2"),
-        (r'"abc\"', "2"),
-        (r'"\xff\xzz"', "5"),
+        ('""', 2),
+        ('"abc"', 2),
+        (r'"aaa\"aaa"', 3),
+        (r'"\x27"', 5),
+        (r'""\n"abc"\n"aaa\"aaa"\n"\x27"', 12),
+        (r'"ab\x34\"five\\"', 7),
+        (r'"\x2bv"', 5),
+        (r'"\"', 2),
+        (r'"abc\"', 2),
+        (r'"\xff\xzz"', 5),
+        (aoc.get_input(2015, 8), 1342),
     ],
 )
 def test_char_diff(santa_list, result):
--- 2015/d8p2.py
+++ 2015/d8p2.py
@@ -9,14 +9,15 @@ def encode(line: str) -> str:
     return f'"{re.sub(r'("|\\)', r'\\\1', line)}"'
 
 
-def char_diff(santa_list: str) -> str:
+def char_diff(santa_list: str) -> int:
     code_chars = 0
     encoded_code_chars = 0
     for line in santa_list.strip().splitlines():
         code_chars += len(line)
         encoded_code_chars += len(encode(line))
 
-    return str(encoded_code_chars - code_chars)
+    diff = encoded_code_chars - code_chars
+    return diff
 
 
 if __name__ == "__main__":
@@ -44,10 +45,11 @@ def test_encode(line, encoded):
 @pytest.mark.parametrize(
     "santa_list,result",
     [
-        ('""', "4"),
-        (r'"abc"', "4"),
-        (r'"aaa\"aaa"', "6"),
-        (r'"\x27"', "5"),
+        ('""', 4),
+        (r'"abc"', 4),
+        (r'"aaa\"aaa"', 6),
+        (r'"\x27"', 5),
+        (aoc.get_input(2015, 8), 2074),
     ],
 )
 def test_char_diff(santa_list, result):
