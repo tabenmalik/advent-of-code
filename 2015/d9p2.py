ba9c56d83d654c0e12e4e6252597e77ba18da689
Taben Malik <tabenjmalik@gmail.com>
2026-04-19 15:52:21 -0500

    solve 2015 day 9 part 2

---
+++ 2015/d9p2.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import re
+from itertools import pairwise
+from itertools import permutations
+
+import aoc
+
+RE_DISTANCE = re.compile(r"(\w+) to (\w+) = (\d+)")
+
+# sparse graph representation where the key is a pair
+# of locations (i.e. identifies an edge between the nodes)
+# and the value is the distance between the locations
+Network = dict[tuple[str, str], int]
+
+
+def parse_distances(distances: str) -> Network:
+    network: Network = {}
+
+    for line in distances.strip().splitlines():
+        m = RE_DISTANCE.fullmatch(line)
+        assert m is not None
+        network[(m.group(1), m.group(2))] = int(m.group(3))
+        # add second entry to make downstream logic easier.
+        # this datastructure acts like a directed graph
+        # but if there's a back and forth edge then it
+        # seems like an undirected graph.
+        network[(m.group(2), m.group(1))] = int(m.group(3))
+
+    return network
+
+
+def distance_of_route(route: tuple[str, ...], network: Network) -> int:
+    distance = 0
+    for edge in pairwise(route):
+        distance += network[edge]
+    return distance
+
+
+def find_longest_distance(distances: str) -> int:
+    network = parse_distances(distances)
+
+    # brute force
+    all_locations = {key[0] for key in network}
+    all_route_distances = (
+        distance_of_route(route, network) for route in permutations(all_locations)
+    )
+    longest_distance = max(all_route_distances)
+    return longest_distance
+
+
+if __name__ == "__main__":
+    raise SystemExit(
+        aoc.problem_entry_point(find_longest_distance, 2015, 9),
+    )
+
+
+import pytest  # noqa: E402
+
+
+@pytest.mark.parametrize(
+    "distances,longest_distance",
+    [
+        (
+            "London to Dublin = 464\n"
+            "London to Belfast = 518\n"
+            "Dublin to Belfast = 141\n",
+            982,
+        ),
+        (aoc.get_input(2015, 9), 909),
+    ],
+)
+def test_find_longest_distance(distances, longest_distance):
+    assert longest_distance == find_longest_distance(distances)
