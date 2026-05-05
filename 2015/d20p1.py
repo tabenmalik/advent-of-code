ce0a9a459aa7ebffa2803a0bf06c5bd8607e5585
Taben Malik <tabenjmalik@gmail.com>
2026-05-04 19:07:16 -0500

    improve performance of 2015 day 20 part 1 by checking only even numbers

--- 2015/d20p1.py
+++ 2015/d20p1.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Iterator
+from itertools import chain
 from itertools import count
 from math import isqrt
 
@@ -8,7 +9,7 @@ import aoc
 
 
 def divisors(n: int) -> Iterator[int]:
-    """Return all divisors of `n` in no particular order."""
+    """Return all divisors of `n`. Order not guaranteed."""
     for i in range(1, isqrt(n) + 1):
         if n % i == 0:
             yield i
@@ -18,13 +19,28 @@ def divisors(n: int) -> Iterator[int]:
 
 
 def presents(house: int) -> int:
+    """Return the presents that `house` will receive."""
     return sum(divisors(house)) * 10
 
 
 def solve(inp: str) -> int:
     target_presents = int(inp)
 
-    for house in count(1):
+    # Limit the search to only even numbers after house 3.
+    # In effect, only the highly abundant numbers need to be
+    # checked. It's not practical to generate the sequence
+    # of highly abundant numbers but the numbers to search
+    # can be cut almost in half due to the property that all
+    # highly abundant numbers above 3 are even.
+    # What a time saver!
+    #
+    # Why must the answer be a highly abundant number?
+    # The sum-of-divisors is not a monotonically
+    # increasing sequence. The highly abundant numbers are the
+    # "record" peaks in the sequence, a subset that *is* monotonically
+    # inceasing. So a house with a highly abundant number is guaranteed
+    # to be the first house that has at least X pesents.
+    for house in chain([1, 2, 3], count(4, 2)):
         if presents(house) >= target_presents:
             return house
 
