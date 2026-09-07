45073bf1258f454590c73589af970c1b97343401
Taben Malik <tabenjmalik@gmail.com>
2026-09-06 20:54:35 -0500

    stricter typing for 2025 day 3 part 1 and 2

--- 2025/day03part2.py
+++ 2025/day03part2.py
@@ -1,7 +1,42 @@
 from __future__ import annotations
 
-from aoc.year2025 import day03part1
+from pathlib import Path
 
 
-def solve(input_s, num_batteries=12):
-    return day03part1.solve(input_s, num_batteries=num_batteries)
+def _max_joltage_batteries(bank: str, num_batteries: int) -> str:
+    joltage_index = 0
+    for joltage in range(9, 0, -1):
+        joltage_index = bank.find(
+            str(joltage), 0, len(bank) - num_batteries + 1,
+        )
+        if joltage_index != -1:
+            break
+
+    if num_batteries == 1:
+        return bank[joltage_index]
+    return bank[joltage_index] + _max_joltage_batteries(
+        bank[joltage_index + 1:], num_batteries - 1,
+    )
+
+
+def _max_joltage(bank: str, num_batteries: int) -> int:
+    return int(_max_joltage_batteries(bank, num_batteries))
+
+
+def _total_max_joltage(banks: list[str], num_batteries: int) -> int:
+    return sum(map(lambda bank: _max_joltage(bank, num_batteries), banks))
+
+
+def _read_battery_banks(path: Path) -> list[str]:
+    with open(path) as fobj:
+        return fobj.read().strip().split()
+
+
+def _parse_battery_banks(input_s: str) -> list[str]:
+    return input_s.strip().split()
+
+
+def solve(input_s: str, num_batteries: int = 12) -> int:
+    banks = _parse_battery_banks(input_s)
+    joltage = _total_max_joltage(banks, num_batteries)
+    return joltage
